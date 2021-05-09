fac4ae7005841d1ae40e7c0c17af0b9d93b4c206
Oluwaseyi Olufemi <oluwaseyi.olufemi.e@gmail.com>
2021-05-09 23:57:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-ef fizzbuzz(num)
+def fizzbuzz(num)
   if (num % 5 == 0) && (num % 3 == 0)
     return "FizzBuzz"
   elsif num % 3 == 0
